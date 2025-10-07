dc670d2186571ffd2e4558e889e2e0deb1959654
Shahariya-17 <shahariyarrakib780@gmail.com>
2025-10-07 19:58:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+// Question-1
+const color = ['Red', 'Green', 'Yellow', 'Black', 'White'];
+console.log(color.length);
+
+// Question-2
+const fruits = ['Apple', 'Banana', 'Mango'];
+console.log(fruits.join());
+
+// Question-3
+const letter = ['A', 'B', 'C', 'D'];
+console.log(letter[2]);
+
+// Question-4
+const words = ['Learning', 'JavaScript', 'is', 'fun'];
+console.log(words.join(" "));
+
+// Question-5
+const items1 = ['pen', 'pencil', 'eraser', 'sharpener'];
+items1.pop();
+console.log(items1);
+
+// Question-6
+const items2 = ['pen', 'pencil', 'eraser'];
+items2.push('marker');
+console.log(items2);
+
+// Question-7
+const city = ["Dhaka", "Chittagong", "Khulna"];
+city.shift();
+console.log(city);
+
